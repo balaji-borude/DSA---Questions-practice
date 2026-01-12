0aa65a88d6d1dc9052ee7475994a528d042cfa9f
balaji-borude <balajiborude2503@gmail.com>
2026-01-12 19:21:15 +0530

    Complete the OOPs need more practice

---
+++ OOPS/constructor/Inheritance.java
@@ -0,0 +1,25 @@
+package constructor;
+
+public class Inheritance {
+    public static void main(String args[]){
+        Fish fs = new Fish();
+        fs.eat();
+        fs.color="red";
+        System.out.println(fs.color);
+    }
+    
+}
+
+class Animal{
+    String color;
+    void eat(){
+        System.out.println("eat");
+    };
+
+}
+class Fish extends Animal{  
+    void swim(){
+        System.out.println("Swim") ;
+    }
+}
+
