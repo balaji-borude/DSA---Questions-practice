9d89f9837dd6ad942271d4723898e036c1d77451
balaji-borude <balajiborude2503@gmail.com>
2026-01-16 10:59:02 +0530

    abstraction with abstract class is done

--- String/string.java
+++ String/string.java
@@ -13,6 +13,7 @@ class string {
     public static boolean isPalindrome(String str) {
 
         int n = str.length();
+        
         for (int i = 0; i < n / 2; i++) {
             if (str.charAt(i) != str.charAt(n - 1 - i)) {
                 return false;
